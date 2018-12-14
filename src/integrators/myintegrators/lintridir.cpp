ed8cada65bc457c1c6d8b1b52baa55b771a571c1
linhsu0723 <linhsu0723@qq.com>
2018-12-15 07:01:42 +0800

    debug portal mesh

--- src/integrators/myintegrators/lintridir.cpp
+++ src/integrators/myintegrators/lintridir.cpp
@@ -158,40 +158,77 @@ public:
         //aether::Vector3 tri2_v1{ 0.153878, 1.990000, -0.189689};
         //aether::Vector3 tri2_v2{ 0.153878, 1.990000, 0.139690};
 
-
+        std::vector<std::array<aether::Vector3, 3>> keyhole_tris;
         // veach_door
         aether::Vector3 tri1_v0{121.9446, 0, -73.0101};
         aether::Vector3 tri1_v1{119.1694, 121.5088, -56.649};
         aether::Vector3 tri1_v2{119.1694, 0.5088, -56.649};
+        keyhole_tris.push_back({{tri1_v0, tri1_v1, tri1_v2}});
 
         aether::Vector3 tri2_v0{121.9446, 0, -73.0101};
         aether::Vector3 tri2_v1{121.9446, 123.3378, -73.0101};
         aether::Vector3 tri2_v2{119.1694, 121.5088, -56.649};
+        keyhole_tris.push_back({{tri2_v0, tri2_v1, tri2_v2}});
 
         // Top of opening
         aether::Vector3 tri3_v0{121.9446, 123.3378, -73.0101};
         aether::Vector3 tri3_v1{62.1593, 121.5088, -74.4293};
         aether::Vector3 tri3_v2{119.1694, 121.5088, -56.649};
-
-        // Back of door
-        aether::Vector3 tri4_v0{121.9446, 123.3378, -77.0547};
-        aether::Vector3 tri4_v1{58.7111, 123.3378, -77.0547};
-        aether::Vector3 tri4_v2{58.7111, 0, -77.0547};
-
-        aether::Vector3 tri5_v0{121.9446, 123.3378, -77.0547};
-        aether::Vector3 tri5_v1{58.7111, 0, -77.0547 };
-        aether::Vector3 tri5_v2{121.9446, 0, -77.0547};
-
-        std::vector<std::array<aether::Vector3, 3>> keyhole_tris;
-        keyhole_tris.push_back({{tri1_v0, tri1_v1, tri1_v2}});
-        keyhole_tris.push_back({{tri2_v0, tri2_v1, tri2_v2}});
         keyhole_tris.push_back({{tri3_v0, tri3_v1, tri3_v2}});
-        keyhole_tris.push_back({{tri4_v0, tri4_v1, tri4_v2}});
-        keyhole_tris.push_back({{tri5_v0, tri5_v1, tri5_v2}});
+
+        // // Back of door
+        // aether::Vector3 tri4_v0{121.9446, 123.3378, -77.0547};
+        // aether::Vector3 tri4_v1{58.7111, 123.3378, -77.0547};
+        // aether::Vector3 tri4_v2{58.7111, 0, -77.0547};
+        // keyhole_tris.push_back({{tri4_v0, tri4_v1, tri4_v2}});
+
+        // aether::Vector3 tri5_v0{121.9446, 123.3378, -77.0547};
+        // aether::Vector3 tri5_v1{58.7111, 0, -77.0547 };
+        // aether::Vector3 tri5_v2{121.9446, 0, -77.0547};
+        // keyhole_tris.push_back({{tri5_v0, tri5_v1, tri5_v2}});
+
+        // auto GetArea = [&](std::array<aether::Vector3, 3> &tri) {
+        //     auto a = to_vector(tri[0] - tri[1]);
+        //     auto b = to_vector(tri[0] - tri[2]);
+        //     return 2.f / (a[1] * b[2] + a[2] * b[0] + a[0] * b[1] - a[0] * b[2] - a[1] * b[0] - a[2] * b[1]);
+        // };
+        // for(auto &tri: keyhole_tris)
+        //     cout << GetArea(tri) << endl;
 
         RandomSequence<Vertex> keyholePath;
         AppendKeyhole(keyholePath, uniDist, *m_raycaster, keyhole_tris);
         keyholePath.Sample();
+
+        while(0) {
+            RandomSequence<Vertex> keyholePath;
+            AppendKeyhole(keyholePath, uniDist, *m_raycaster, keyhole_tris);
+            cout << "--------------------" << endl;
+            keyholePath.Sample();
+            exportPath(keyholePath);
+            auto p0 = to_point(keyholePath[0].Value());
+            auto p1 = to_point(keyholePath[1].Value());
+            cout << p0.toString() << endl;
+            cout << p1.toString() << endl;
+            cout << "-----" << endl;
+            Intersection its = keyholePath[0].Get(intersection_);
+            Intersection nextIts = keyholePath[1].Get(intersection_);
+            const mitsuba::Vector toNext = directionToNext(keyholePath[0], keyholePath[1]);
+            // cout << its.geoFrame.n.toString() << endl;
+            // cout << toNext.toString() << endl;
+            // cout << nextIts.geoFrame.n.toString() << endl;
+            const Float nextCosine = nextIts.isValid() ?
+                fabs(dot(-toNext, nextIts.geoFrame.n)) : Float(1.f);
+            const Float distSq = distanceSquared(
+                to_point(keyholePath[0].Value()),
+                to_point(keyholePath[1].Value()));
+            cout << fabs(dot(toNext, its.geoFrame.n)) << " " << 1.f / distSq << " " << nextCosine << endl;
+            Float geometryTerm = fabs(dot(toNext, its.geoFrame.n)) / distSq * nextCosine;
+            cout << geometryTerm << endl;
+            cout << geometryTerm * INV_TWOPI << endl;
+            cout << "----------" << endl;
+            cout << keyholePath.Pdf() << endl;
+            getchar();
+        }
 
         const ref_vector<Emitter> &emitters = scene->getEmitters();
 
@@ -231,6 +268,10 @@ public:
             std::vector<RandomSequence<Vertex>> paths;
             for (int sensorSubpathSize = 1; sensorSubpathSize <= pathLength + 1; sensorSubpathSize++) {
                 const int emitterSubpathSize = pathLength + 1 - sensorSubpathSize;
+
+                if(emitterSubpathSize != 1 || sensorSubpathSize != 3) continue;
+                // if(emitterSubpathSize != 2 || sensorSubpathSize != 2) continue;
+
                 auto sensorSubpathSlice = sensorSubpath.Slice(0, sensorSubpathSize);
                 RandomSequence<Vertex> path;
 
@@ -293,6 +334,131 @@ public:
         }
     }
 
+	void exportPath(const RandomSequence<Vertex> &path) const {
+		std::ofstream ofs("/media/lin/MintSpace/program/CG/PortalFinding/scene/veach_door/path.obj");
+		int k = 0;
+		auto exportSegment = [&](const Vertex &v0, const Vertex &v1) {
+			const Point3 p0 = to_point(v0.Value());
+			const Point3 p1 = to_point(v1.Value());
+			ofs << "v " << p0.x << " " << -p0.z << " " << p0.y << endl;
+			ofs << "v " << p0.x << " " << -p0.z << " " << p0.y << endl;
+			ofs << "v " << p1.x << " " << -p1.z << " " << p1.y << endl;
+			ofs << "f " << k * 3 + 1 << " " << k * 3 + 2 << " " << k * 3 + 3 << endl;
+			k++;
+		};
+
+		for(int i = 0; i < path.Size() - 1; i++)
+			exportSegment(path[i], path[i + 1]);
+	}
+
+inline mitsuba::Vector directionToPrevious(const Vertex &previousVertex, const Vertex &vertex) const {
+    return to_vector((previousVertex.Value() - vertex.Value()).normalized());
+}
+
+inline mitsuba::Vector directionToNext(const Vertex &vertex, const Vertex &nextVertex) const {
+    return to_vector((nextVertex.Value() - vertex.Value()).normalized());
+}
+
+Spectrum Le(const Scene *scene, const Vertex &previousVertex, const Vertex &vertex) const {
+    const Emitter *emitter = vertex.Get(emitter_);
+    const Intersection &its = vertex.Get(intersection_);
+    const mitsuba::Vector directionToLight = -directionToPrevious(previousVertex, vertex);
+    if (emitter != nullptr) {
+        // Sampled emitter
+        return emitter->eval(its, -directionToLight);
+    }
+    if (its.isValid()) {
+        // if its is an emitter, emitter shouldn't be nullptr
+        SAssert(!its.isEmitter());
+        if (its.isEmitter()) {
+            // Hit an area light
+            return its.Le(-directionToLight);
+        }
+        // Hit a non-emitter object
+        return Spectrum(0.f);
+    }
+    // Hit background
+    // It might seem not making sense using its.p, but evalEnvironment doesn't use ray.o anyway
+    return scene->evalEnvironment(RayDifferential(its.p, directionToLight, its.time));
+}
+
+Spectrum Eval(const Scene *scene, const RandomSequenceView<Vertex> &path, const int index) const {
+    SAssert(path.Size() > 1);
+    SAssert(index >= 1);
+    if (index == path.Size() - 1) {
+        return Le(scene, path[index - 1], path[index]);
+    }
+
+    SAssert(index + 1 < path.Size());
+    Intersection its = path[index].Get(intersection_);
+    if (!its.isValid()) {
+        // Not a valid surface
+        return Spectrum(0.f);
+    }
+    SAssert(its.shape != nullptr);
+    its.wi = its.toLocal(directionToPrevious(path[index - 1], path[index]));
+    const mitsuba::Vector toNext = directionToNext(path[index], path[index + 1]);
+    BSDFSamplingRecord bRec(its, its.toLocal(toNext));
+    bRec.framePerturbed = true;
+    // TODO: support ray differentials
+    const BSDF *bsdf = its.getBSDF();
+    SAssert(bsdf != nullptr);
+    const Spectrum bsdfValue = bsdf->eval(bRec);
+    if (bsdfValue.isZero()) {
+        return Spectrum(0.f);
+    }
+    const Intersection &nextIts = path[index + 1].Get(intersection_);
+    const Float nextCosine = nextIts.isValid() ?
+        fabs(dot(-toNext, nextIts.geoFrame.n)) : Float(1.f);
+    const Float distSq =
+        distanceSquared(to_point(path[index].Value()),
+                        to_point(path[index + 1].Value()));
+    if (distSq == Float(0)) {
+        return Spectrum(0.f);
+    }
+    // bsdf->eval already includes one of the cosines
+    const Float geometryTerm = nextCosine / distSq;
+    if (!std::isfinite(geometryTerm) || !bsdfValue.isValid()) {
+        return Spectrum(0.f);
+    }
+
+    cout << "    " << index << " " << fabs(dot(toNext, its.geoFrame.n))
+         << " " << 1.f / distSq << " " << nextCosine << endl;
+    return geometryTerm * bsdfValue * Eval(scene, path, index + 1);
+}
+
+Spectrum EvalSensor(const Scene *scene, const RandomSequenceView<Vertex> &path) const {
+    // Should we obtain sensor from the path?
+    const Sensor *sensor = scene->getSensor();
+    SAssert(path[0].Get(sensor_) != nullptr);
+    PositionSamplingRecord pRec(path[1].Get(intersection_).time);
+    pRec.p = to_point(path[0].Value());
+    DirectionSamplingRecord dRec;
+    dRec.d = to_vector((path[1].Value() - path[0].Value()).normalized());
+    dRec.measure = ESolidAngle;
+    Spectrum We = sensor->evalDirection(dRec, pRec);
+    const Intersection &nextIts = path[1].Get(intersection_);
+    const Float nextCosine = nextIts.isValid() ?
+        fabs(dot(-dRec.d, nextIts.geoFrame.n)) : Float(1.f);
+    // bsdf->eval already includes one of the cosines
+    const Float geometryTerm = nextCosine /
+        distanceSquared(to_point(path[0].Value()),
+                        to_point(path[1].Value()));
+
+    cout << "    " << 0 << " " << geometryTerm / nextCosine << " " << nextCosine << endl;
+    return geometryTerm * We;
+}
+
+TSpectrum<double, SPECTRUM_SAMPLES> EvalBidir(const Scene *scene, const RandomSequenceView<Vertex> &path) const {
+    SAssert(path.AllValid());
+    if (path.Size() <= 1) {
+        // Need to at least have a sensor and an emitter
+        return TSpectrum<double, SPECTRUM_SAMPLES>(0.f);
+    }
+
+    return TSpectrum<double, SPECTRUM_SAMPLES>(EvalSensor(scene, path) * Eval(scene, path, 1));
+}
+
     void estimate(const Scene *scene,
                   ImageBlock *imageBlock,
                   const std::vector<RandomSequence<Vertex>> &paths,
@@ -330,6 +496,46 @@ public:
                 double pdf = paths[j].Pdf(path);
                 pdfs.push_back(pdf);
             }
+
+
+
+            #define OUTPUT_VERTEX_PDF
+            #if defined OUTPUT_VERTEX_PDF
+            cout << "--------------------" << endl;
+            cout << paths.size() << " " << i << endl;
+
+            SourceDistributionHelper source_distribution_helper;
+
+            Real p = 1;
+            using T = typename RandomSequence<Vertex>::sample_t::optional_element_t;
+
+            std::size_t sample_index = 0;
+            for (std::size_t strategy_index = 0, N = paths[0].strategies.Size(); strategy_index < N; ++strategy_index) {
+              const auto& strategy = paths[0].strategies[strategy_index];
+              std::vector<T> query_samples;
+              query_samples.reserve(strategy->output_size);
+              for (std::size_t i = 0; i < strategy->output_size; ++i) {
+                query_samples.push_back(path.store.at(sample_index + i).get().rv);
+              }
+
+              auto pdf = strategy->Pdf(path, query_samples, sample_index,
+                                       paths[0].reversed[sample_index], source_distribution_helper);
+              if (pdf == 0 || std::isnan(pdf)) {
+                break;
+              }
+              // if(sample_index == 1)
+              cout << "    " << sample_index << " " << pdf << endl;
+              p *= pdf;
+              sample_index += strategy->output_size;
+            }
+            cout << p << endl;
+            exportPath(path);
+            auto Li = EvalBidir(scene, make_view(path));
+            getchar();
+            #endif
+
+
+
             double weight = misWeight(i, pdfs);
             Spectrum weightedContribution = weight * contribution;
             int pathLength = path.Size() - 1;
